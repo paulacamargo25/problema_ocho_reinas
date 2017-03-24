c02ce290ead1b5d857e998c14a18906738c22ef9
paulacamargo25 <paula@inka-labs.com>
2017-03-24 12:40:52 -0500

    Cambiar titulo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Problema de las ocho reinas' });
 });
 
 module.exports = router;
